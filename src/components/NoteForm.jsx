ec483057c8fa432e22c1d5791f9f6aac222797df
Sule Bello <sbello1234@gmail.com>
2025-09-29 15:25:12 -0400

    here

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,13 +1,44 @@
 import { useState } from "react";
 const NoteForm = () => {
     const [title, SetTitle] = useState('');
+    const [priority, SetPriority] = useState('Medium');
+    const [category, SetCategory] = useState('Work');
+    const [description, SetDescription] = useState('');
     return <form className="mb-6">
         <div className="mb-4">
             <label htmlFor="title" className="block font-semibold">
                 Title
             </label>
-            <input type="text" className="w-full p-2 border rounded-lg" value={title} onChange={() => SetTitle()} />
+            <input type="text" className="w-full p-2 border rounded-lg" value={title} onChange={() => SetTitle(e.target.value)} />
         </div>
+            <div className="mb-4">
+            <label htmlFor="priority" className="block font-semibold">
+                priority
+            </label>
+            <select type="text" className="w-full p-2 border rounded-lg" value={priority} onChange={() => SetTitle(e.target.value)} >
+            <option value='High'>High</option>
+              <option value='Medium'>Medium</option>
+                <option value='Low'>Low</option>
+
+                 </select>
+        </div>
+             <div className="mb-4">
+            <label htmlFor="category" className="block font-semibold">
+                priority
+            </label>
+            <select type="text" className="w-full p-2 border rounded-lg" value={category} onChange={() => SetTitle(e.target.value)} > 
+            <option value='Work'>Work</option>
+              <option value='Personal'>Personal</option>
+                <option value='Ideas'>Ideas</option>
+                </select>
+        </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-semibold">
+            Description
+            </label>
+            <textarea type="text" className="w-full p-2 border rounded-lg" value={description} onChange={() => SetTitle(e.target.value)} ></textarea>
+        </div>
+        <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer">Create Note</button>
     </form>
 };
  
